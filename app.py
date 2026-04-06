5fcc4eb9123f2ae880ead25b43f4441ca39e1d5f
Ridwan Kazi <ridwank58@kprschools.ca>
2026-04-06 22:12:59 +0000

    menu

--- app.py
+++ app.py
@@ -1,5 +1,14 @@
 import database
 
+MENU_PROMPT = """ -- Coffee Bean App --
+
+Please choose one of these options:
+
+1) Add a new bean
+2) See all beans.
+3) Find a bean by name.
+4) See which preparation method is best for a bean.
+"""
 
 def menu():
     connection = database.connect()
